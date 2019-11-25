d334be1a6896d97306320fd82287758996331521
AKSHAY VARMA <akshayvarma10@gmail.com>
2019-11-26 01:41:47 +0530

    Added loader on btn click, added api call for next image

--- orderfood/static/modal.js
+++ orderfood/static/modal.js
@@ -1,15 +1,48 @@
 $(document).ready(function() {
-    $('#yes').click(function() {
-        callApi('yes');
-    });
-
-    $('#no').click(function() {
-        callApi('no');
-    });
+    $('#yes').click(() => handleBtnClick('yes'));
+    $('#no').click(() => handleBtnClick('no'));
 });
 
-function callApi(path) {
-    $.ajax({
-        url: '/minder/' + path
+function handleBtnClick(actionPath) {
+  toggleLoaderInBtn('#' + actionPath, true);
+  setBtnsDisable(true);
+
+  callModalActionApi(actionPath)
+    .then(function(resp) {
+      console.log(resp);
+      return getNextImage();
+    })
+    .then(function(imageData) {
+      // TODO: set new image in card
+    })
+    .catch(function(err) {
+      console.log('Some error occurred', err);
+    })
+    .always(function() {
+      toggleLoaderInBtn('#' + actionPath, false);
+      setBtnsDisable(false);
     });
+}
+
+function setBtnsDisable(isDisabled) {
+  $("#action-btns").find(".btn").attr('disabled', isDisabled);
+}
+
+function toggleLoaderInBtn(btnSelector, showLoader) {
+  let $element = $(btnSelector);
+  const classNames = 'animated loading loading-white u-center hide-text';
+  if (showLoader) {
+    $element.addClass(classNames);
+  } else {
+    $element.removeClass(classNames);
+  }
+}
+
+function callModalActionApi(path) {
+    return $.get('/minder/' + path);
+}
+
+function getNextImage() {
+    // TODO: add api url for get image
+    return $.get('/');
 }
