7ded6f03ce167ade2eb9653654d5be874a356535
harsh.ku <harsh.ku@media.net>
2019-12-03 01:46:45 +0530

    imports json lib to fix bug

--- orderfood/minder.py
+++ orderfood/minder.py
@@ -5,6 +5,7 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify
 )
 import requests
+import json
 
 bp = Blueprint('minder', __name__, url_prefix='/minder')
 
