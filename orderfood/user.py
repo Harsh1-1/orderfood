9c8f9510fb7d061a91e1126ce97502b5a0f73a65
harsh.ku <harsh.ku@media.net>
2019-12-05 22:19:29 +0530

    updates user interest api

--- orderfood/user.py
+++ orderfood/user.py
@@ -2,10 +2,20 @@ from flask import (
     Blueprint, flash, g, redirect, jsonify, render_template, request, session, url_for
 )
 
+from .db import MySQL
+
 bp = Blueprint('user', __name__, url_prefix='/user')
 
 @bp.route("/interest", methods=['POST'])
 def interest():
     userId = request.args.get('userId')
     interest = request.args.get('interest')
-    return "yay you have installed food app"
+
+    update_query = "UPDATE app_users SET interest = '" + str(interest) + "' where user_id = '" + str(userId) + "';"
+        mysql = MySQL()
+        db_meta = mysql.load_db_meta()
+        mydb = mysql.create_mysql_db_object(db_meta['host'],db_meta['username'], db_meta['password'], db_meta['port'], db_meta['db'])
+        mysql.cudOperations(mydb, update_query)
+        mydb.close()
+
+    return "200"
